91d9afbb5cd6b55c90f2be6c61899702267ac1a2
Ernesto Rojas <ernesto20145@gmail.com>
2024-01-29 22:58:34 -0300

    Added class 33 and more

--- 33-versiones-y-paquetes/yarn-demo/app.js
+++ 33-versiones-y-paquetes/yarn-demo/app.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import operations from 'my-440-calculator-package';
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello from yarn project 😍');
+});
+
+app.get('/sum/:a/:b', (req, res) => {
+  const { params: { a, b } } = req;
+  res.send(`El resultado de la suma ${a} + ${b} es: ${operations.sum(parseInt(a), parseInt(b))} 😍`);
 });
 
 app.listen(8080, () => {
